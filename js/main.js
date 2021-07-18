d2196a18c0d46fccb32e8782edc8464cdbf47b77
KoshiroNishide <nishideko0824@gmail.com>
2021-07-18 10:19:22 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
 AOS.init({
   offset: 100,
-<<<<<<< HEAD
   duration: 600,
   easing: 'ease',
   delay: 500,
-=======
-  duration: 800,
-  easing: 'ease',
-  delay: 600,
->>>>>>> a65ace43676cca7c750563a4240f4c758be0092c
   once: false,
 	mirror: false,
   anchorPlacement: 'top-center',
@@ -41,7 +35,6 @@ document.getElementById('hamburger').addEventListener('click',() => {
   hamburger();
 });
 
-<<<<<<< HEAD
 const pagetopBtn = document.querySelector('#page-top');
 pagetopBtn.addEventListener('click', () => {
   window.scrollTo({
@@ -49,5 +42,3 @@ pagetopBtn.addEventListener('click', () => {
     behavior: "smooth"
   });
 });
-=======
->>>>>>> a65ace43676cca7c750563a4240f4c758be0092c
