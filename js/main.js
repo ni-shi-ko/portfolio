61376f7848ca2979b3ebf3b814e1fcb3923d872b
KoshiroNishide <nishideko0824@gmail.com>
2021-08-02 16:15:44 +0900

    update

--- js/main.js
+++ js/main.js
@@ -11,19 +11,25 @@ AOS.init({
   animatedClassName: 'animated',
 });
 
-const img_src = ['./img/fashion.jpg', './img/momizi.jpg', './img/tokyostation.jpg', './img/train.jpg', './img/nara.jpg'];
+const img_src = ['./img/fashion.jpg', './img/momizi.jpg', './img/tokyostation.jpg', './img/train.jpg', './img/nara.jpg', './img/azisai.png'];
 let num = -1;
 
+const img_src_sec = ['./img/my1.jpg', './img/my2.jpg', './img/my3.jpg', './img/my4.jpg', './img/my5.jpg', './img/my6.jpg'];
+
+const img_src_thi = ['./img/cafe1.jpg', './img/cafe2.jpg', './img/cafe3.jpg', './img/cafe4.jpg', './img/cafe5.jpg', './img/cafe6.jpg' ];
+
 function slide_time() {
-  if (num === 4) {
+  if (num === 5) {
     num = 0;
   } else {
     num++;
   }
   document.getElementById("hobby-slide").src = img_src[num];
+  document.getElementById("fashion-slide").src = img_src_sec[num];
+  document.getElementById("cafe-slide").src = img_src_thi[num];
 }
 
-setInterval(slide_time, 2000);
+setInterval(slide_time, 1200);
 
 function hamburger() {
   document.getElementById('line1').classList.toggle('line_1');
